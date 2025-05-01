6528b4b412a4f6f6fceb7eb4096e44ab7166a9d1
Jbobb12 <jb3179@georgetown.edu>
2025-05-01 11:12:50 -0400

    push

---
+++ emails.py
@@ -0,0 +1,3 @@
+ALLOWED_EMAILS = ['an922@georgetown.edu', 'jb3179@georgetown.edu', 'crm199@georgetown.edu', 
+                  'gsl39@georgetown.edu', 'km1897@georgetown.edu', 'krv14@georgetown.edu', 
+                  'michellealioto@dcsoccerclub.org']
